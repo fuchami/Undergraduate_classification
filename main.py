d6785dc3a0f84dca0b92837a739d50284f2c1923
fuchami <famichiki.yuuki@gmail.com>
2019-02-19 23:50:07 +0900

    update

--- main.py
+++ main.py
@@ -117,17 +117,15 @@ def pred(img, pred_model):
     # 予測
     print('予測')
     pred = pred_model.predict(img, batch_size=1)
+    del img
+    gc.collect()
     score = np.max(pred)
     pred_label = np.argmax(pred)
     print(pred_label)
 
     if pred_label == 0:
-        del pred,img
-        gc.collect()
         return 0, score
     else:
-        del pred,img
-        gc.collect()
         return 1 ,score
 
 # LINEから画像データを取得
